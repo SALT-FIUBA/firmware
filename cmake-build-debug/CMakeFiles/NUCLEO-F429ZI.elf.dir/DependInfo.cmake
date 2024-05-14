349a1ab44fc961ec35896951794624b275ad03a0
Fernando Iglesias <figlesias@wbuild.io>
2024-05-14 17:51:44 -0300

    qpc blinky example on stm32f429zi-nucleo-144

---
+++ cmake-build-debug/CMakeFiles/NUCLEO-F429ZI.elf.dir/DependInfo.cmake
@@ -0,0 +1,95 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/nando/NUCLEO-F429ZI/Core/Startup/startup_stm32f429zitx.s" "/Users/nando/NUCLEO-F429ZI/cmake-build-debug/CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Startup/startup_stm32f429zitx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/nando/NUCLEO-F429ZI/Core/Inc"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/CMSIS/Include"
+  "/Users/nando/NUCLEO-F429ZI/qpc/include"
+  "/Users/nando/NUCLEO-F429ZI/qpc/ports/arm-cm/qk/gnu"
+  "/Users/nando/NUCLEO-F429ZI/qpc/bsp"
+  "/Users/nando/NUCLEO-F429ZI/qpc/blinky"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/eth.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/eth.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/eth.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/gpio.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/gpio.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/main.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/stm32f4xx_it.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/syscalls.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/sysmem.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/system_stm32f4xx.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/usart.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/usart.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Core/Src/usb_otg.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/usb_otg.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Core/Src/usb_otg.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_eth.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_eth.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_eth.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/blinky/blinky.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/blinky/blinky.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/blinky/blinky.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/bsp/bsp.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/bsp/bsp.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/bsp/bsp.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/bsp/stm32f4xx_nucleo_144.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/bsp/stm32f4xx_nucleo_144.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/bsp/stm32f4xx_nucleo_144.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/ports/arm-cm/qk/gnu/qk_port.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/ports/arm-cm/qk/gnu/qk_port.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/ports/arm-cm/qk/gnu/qk_port.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qep_hsm.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qep_hsm.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qep_hsm.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qep_msm.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qep_msm.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qep_msm.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_act.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_act.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_act.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_actq.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_actq.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_actq.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_defer.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_defer.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_defer.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_dyn.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_dyn.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_dyn.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_mem.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_mem.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_mem.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_ps.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_ps.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_ps.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_qact.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_qact.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_qact.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_qeq.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_qeq.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_qeq.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_qmact.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_qmact.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_qmact.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qf/qf_time.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_time.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qf/qf_time.c.obj.d"
+  "/Users/nando/NUCLEO-F429ZI/qpc/src/qk/qk.c" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qk/qk.c.obj" "gcc" "CMakeFiles/NUCLEO-F429ZI.elf.dir/qpc/src/qk/qk.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
